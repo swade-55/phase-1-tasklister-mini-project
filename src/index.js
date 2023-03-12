5b7f497cc85dc8aff5caa70a0e5cb64b5f57c9b9
SamuelWade <samuelrcwade@gmail.com>
2023-03-12 10:06:12 -0400

    lab completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded',()=> {
+  document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    handleToDo(e.target.new_task_description.value);
+  });
+})
+
+function handleToDo(todo){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = 'x';
+  p.textContent = `${todo} `;
+  p.appendChild(btn);
+  document.querySelector('#list').appendChild(p);
+  console.log(p);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
